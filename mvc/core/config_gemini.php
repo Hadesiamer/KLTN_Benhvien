32a90688f53f3aff507a8fe9be343535fc977764
unknown <2003.nhatcuong@gmail.com>
2025-12-04 21:49:56 +0700

    Fix

--- mvc/core/config_gemini.php
+++ mvc/core/config_gemini.php
@@ -3,7 +3,7 @@
 // Cấu hình cho Gemini API dùng trong dự án bệnh viện
 
 // KHÔNG nên commit file này lên repo public.
-define('GEMINI_API_KEY', 'AIzaSyDJPoLocJgD6-ZGe97Hv2w2riUak_EOXaE');
+define('GEMINI_API_KEY', 'AIzaSyDJ.....');
 
 // Model mặc định sử dụng cho chatbot
 define('GEMINI_MODEL_ID', 'gemini-2.5-flash');
